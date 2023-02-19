175d42a14b2ff703d2551f767f792f55c8006627
Tanner Gill <tanner.gill@agilebits.com>
2023-02-19 10:32:46 -0800

    Can update texture of terrain when mesh heights are being calculated

--- Assets/Scripts/TerrainChunk.cs
+++ Assets/Scripts/TerrainChunk.cs
@@ -7,40 +7,79 @@ public class TerrainChunk : MonoBehaviour
     // length of chunk in quads
     public int chunkLength = 100;
     public float scale = 1f;
-    
+
     private Vector3[] chunkVertices;
     private int[] chunkTriangles;
     private Vector3[] chunkNormals;
 
+    private Noise terrainNoise;
+    private Texture2D terrainTexture;
+    private MeshRenderer meshRenderer;
+
     void Start()
     {
         // determine how many vertices we need for the specified chunk size
-        // 2 polygons per quad
-        int polygonRowCount = chunkLength * 2;
-        // 3 vertices per polygon
-        int verticesRowCount = polygonRowCount * 3;
-        
-        int totalVerticesCount = verticesRowCount * chunkLength;
-        Debug.Log(totalVerticesCount);
+        InitializeMeshStorage();
 
-        // create new arrays to contain all of our vertices
-        chunkVertices = new Vector3[totalVerticesCount];
-        chunkTriangles = new int[totalVerticesCount];
-        chunkNormals = new Vector3[totalVerticesCount];
+        // initialize map texture
+        terrainTexture = new Texture2D(chunkLength, chunkLength, TextureFormat.ARGB32, true);
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer.material.mainTexture = terrainTexture;
+
+        // initialize noise
+        terrainNoise = new Noise();
+        terrainNoise.addChannel(
+            new Channel(
+                "Height",
+                Algorithm.Turbulence3d,
+                150.0f,
+                NoiseStyle.Linear,
+                0.0f,
+                1.0f,
+                Edge.Smooth
+            ).setFractal(4, 2.0f, 0.5f)
+        );
 
         // populate arrays with vertices
         int iterations = 0;
-        for (int x = 0; x < chunkLength; x++) {
-            for (int z = 0; z < chunkLength; z++) {
-                Quad quad = new Quad(new Vector3(x, 0, z), iterations, scale);
-                for (int q = 0; q < 6; q++) {
+        for (int x = 0; x < chunkLength; x++)
+        {
+            for (int z = 0; z < chunkLength; z++)
+            {
+                Quad quad = new Quad(new Vector3(x, 0, z), iterations, terrainNoise, scale);
+                for (int q = 0; q < 6; q++)
+                {
                     chunkVertices[iterations] = quad.vertices[q];
                     chunkTriangles[iterations] = quad.triangles[q];
                     chunkNormals[iterations] = quad.normals[q];
                     iterations++;
                 }
+                // TODO: move height calculation for verts up here too
+                // float sample = terrainNoise.getNoise(new Vector3(x, 0, z), "Height") * scale;
+                terrainTexture.SetPixel(x, z, Color.red);
             }
         }
+
+        ApplyUpdatesToMesh();
+    }
+
+    void InitializeMeshStorage()
+    {
+        // 2 polygons per quad
+        int polygonRowCount = chunkLength * 2;
+        // 3 vertices per polygon
+        int verticesRowCount = polygonRowCount * 3;
+
+        int totalVerticesCount = verticesRowCount * chunkLength;
+
+        // create new arrays to contain all of our vertices
+        chunkVertices = new Vector3[totalVerticesCount];
+        chunkTriangles = new int[totalVerticesCount];
+        chunkNormals = new Vector3[totalVerticesCount];
+    }
+
+    void ApplyUpdatesToMesh()
+    {
         Mesh mesh = new Mesh();
         mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
         mesh.vertices = chunkVertices;
@@ -49,29 +88,34 @@ public class TerrainChunk : MonoBehaviour
 
         GetComponent<MeshFilter>().mesh = mesh;
         GetComponent<MeshCollider>().sharedMesh = mesh;
+
+        terrainTexture.Apply();
     }
 }
 
-
-class Quad {
+class Quad
+{
     public Vector3[] vertices;
     public int[] triangles;
     public Vector3[] normals;
 
-    public Quad(Vector3 root, int meshIndex, float scale) {
-        Vector3[] v = new Vector3[] { 
-            this.GetVertPosition(root.x + 1, root.z + 1, scale),
-            this.GetVertPosition(root.x + 1, root.z, scale),
-            this.GetVertPosition(root.x, root.z + 1, scale),
-            this.GetVertPosition(root.x, root.z + 1, scale),
-            this.GetVertPosition(root.x + 1, root.z, scale),
-            this.GetVertPosition(root.x, root.z, scale) 
+    public Quad(Vector3 root, int meshIndex, Noise terrainNoise, float scale)
+    {
+        Vector3[] v = new Vector3[]
+        {
+            this.GetVertPosition(root.x + 1, root.z + 1, terrainNoise, scale),
+            this.GetVertPosition(root.x + 1, root.z, terrainNoise, scale),
+            this.GetVertPosition(root.x, root.z + 1, terrainNoise, scale),
+            this.GetVertPosition(root.x, root.z + 1, terrainNoise, scale),
+            this.GetVertPosition(root.x + 1, root.z, terrainNoise, scale),
+            this.GetVertPosition(root.x, root.z, terrainNoise, scale)
         };
 
         // populate triangles and normals
         int[] t = new int[6];
         Vector3[] n = new Vector3[6];
-        for (int i = 0; i < 6; i++) {
+        for (int i = 0; i < 6; i++)
+        {
             t[i] = meshIndex + i;
             n[i] = Vector3.right;
         }
@@ -81,8 +125,9 @@ class Quad {
         this.normals = n;
     }
 
-    private Vector3 GetVertPosition(float x, float z, float scale) {
-        float height = Mathf.PerlinNoise(x * 0.7f, z * 0.7f) * scale;
+    private Vector3 GetVertPosition(float x, float z, Noise terrainNoise, float scale)
+    {
+        float height = terrainNoise.getNoise(new Vector3(x, 0, z), "Height") * scale;
         return new Vector3(x, height, z);
     }
 }
