0c12ce7d65a704ddffc2d0eaf381a0c59d7d7cf7
Tanner Gill <tanner.gill@agilebits.com>
2023-02-24 16:43:06 -0800

    Decent player movement

--- Assets/Scripts/player/PlayerMovement.cs
+++ Assets/Scripts/player/PlayerMovement.cs
@@ -4,24 +4,39 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    Rigidbody rb;
+    public float movementSpeed = 1f;
+    public float turningSpeed = 1f;
 
-    float currentSpeed = 0f;
+    float speedIncreaseRate = 1000f;
+    Vector3 currentVelocity = new Vector3(0, 0, 0);
+
+    Rigidbody rb;
+    CharacterController controller;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        rb.constraints =
-            RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationX;
-        ;
+        controller = GetComponent<CharacterController>();
+        LockRotation();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            Debug.Log("W");
-        }
+        float turn = Input.GetAxis("Horizontal") * turningSpeed * Time.deltaTime;
+        transform.TransformDirection(new Vector3(0, 0, turn));
+        transform.Rotate(0, turn, 0);
+        float move = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
+        //rb.AddForce(Vector3.back * move);
+        transform.Translate(0, 0, move);
+    }
+
+    void LockRotation()
+    {
+        rb.constraints =
+            RigidbodyConstraints.FreezeRotationZ
+            | RigidbodyConstraints.FreezeRotationX
+            | RigidbodyConstraints.FreezeRotationY;
+        ;
     }
 }
---
+++ README.md
@@ -0,0 +1,8 @@
+## Small World
+
+A work in progress concept.
+
+### Credits
+
+Camera Movement
+https://catlikecoding.com/unity/tutorials/movement/orbit-camera/
